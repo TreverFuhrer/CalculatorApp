274ded2e617ec04af83bf87d11ad9047ddd1e283
toki <clevertrever31@gmail.com>
2024-12-25 10:57:25 -0600

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Toki on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by Toki on 12/25/24.
+//
+
+import Testing
+@testable import CalculatorApp
+
+struct CalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
